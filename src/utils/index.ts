e51ae5b456452d4be3f9b118a35909575ac91793
Laura Cole <lauracole1900@comcast.net>
2021-11-28 01:06:02 -0700

    deleted graphql files

--- src/utils/index.ts
+++
@@ -1,3 +0,0 @@
-export type { Entry } from "./interfaces";
-export { CREATE_ENTRY, DELETE_ENTRY, EDIT_ENTRY } from "./mutations";
-export { QUERY_ALL_ENTRIES, QUERY_ONE_ENTRY, QUERY_ME } from "./queries";
--- src/utils/interfaces.ts
+++
@@ -1,8 +0,0 @@
-
-export interface Entry {
-  _id: String,
-  title: string,
-  content: string,
-  tags: Array<string>,
-  created_At: Date
-}
