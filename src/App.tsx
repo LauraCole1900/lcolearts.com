573062d694e152b83d27712cff09e5d39b39ad8d
Laura Cole <lauracole1900@comcast.net>
2022-05-02 21:10:45 -0600

    working on getting footer to render on everything EXCEPT landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { ReactElement } from "react";
+import { ReactElement } from "react";
 import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Container from "react-bootstrap/Container";
@@ -8,29 +8,35 @@ import Footer from "./components/footer";
 
 
 const App = (): ReactElement => {
+  const urlArray: string[] = window.location.href.split("/");
+  const urlWhere: string | undefined = urlArray.at(-1);
+  console.log({ urlWhere });
 
 
   return (
     <>
-        <Router>
-          <Navigation />
-          <Container fluid className="mycontainer">
-            <Routes>
-              <Route path="/about" element={<About />} />
-              <Route path="/webdev" element={<WebPortfolio />} />
-              <Route path="/musician" element={<Musician />} />
-              <Route path="/martial_artist" element={<MartialArtist />} />
-              <Route path="/contact" element={<Contact />} />
-              <Route path="/" element={<Landing />} />
-              <Route path="*" element={<NotFound />} />
-            </Routes>
-          </Container>
-          <div>
+      <Router>
+        <Navigation />
+        <Container fluid className="mycontainer">
+          <Routes>
+            <Route path="/about" element={<About />} />
+            <Route path="/webdev" element={<WebPortfolio />} />
+            <Route path="/musician" element={<Musician />} />
+            <Route path="/martial_artist" element={<MartialArtist />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/" element={<Landing />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </Container>
+        {urlWhere !== ""
+          ? <><div>
             <p>&nbsp;</p>
             <p>&nbsp;</p>
           </div>
-          <Footer />
-        </Router>
+            <Footer />
+          </>
+          : null}
+      </Router>
     </>
   );
 }
--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,4 +1,4 @@
-import { ReactElement } from "react";
+import { ReactElement, useEffect, useState } from "react";
 import { Col, Image, Row } from "react-bootstrap";
 import { Email, GitHubLight, LinkedIn, Phone, Resume } from "../../pix";
 import "./style.css";
@@ -6,29 +6,40 @@ import "./style.css";
 
 const Footer = (): ReactElement => {
 
-  
+  // const urlArray: string[] = window.location.href.split("/");
+  // const urlWhere: string | undefined = urlArray.at(-1);
+
+  // const [render, setRender] = useState<boolean>(false);
+
+  // useEffect((): void => {
+  //   urlWhere !== "" ? setRender(true) : setRender(false);
+  // }, [render, urlWhere]);
+
+
   return (
     <div className="footer">
-      <Row>
-        <Col sm={{span: 1, offset: 6}} className="center">
-          <p>Contact me:</p>
-        </Col>
-        <Col xs={1}>
-          <a href="mailto:lauracole1900@comcast.net" data-toggle="popover" title="Email me: lauracole1900@comcast.net"><Image fluid src={Email} alt="Email icon" className="icon" /></a>
-        </Col>
-        <Col xs={1}>
-          <a href="tel:970-353-0872" data-toggle="popover" title="Call me: 970-353-0872"><Image fluid src={Phone} alt="Phone icon" className="icon" /></a>
-        </Col>
-        <Col xs={1}>
-          <a href="https://www.linkedin.com/in/laura-cole-3661b01b9/?trk=profile-badge" rel="noreferrer noopener" target="_blank" data-toggle="popover" title="My LinkedIn"><Image fluid src={LinkedIn} alt="LinkedIn icon" className="icon" /></a>
-        </Col>
-        <Col xs={1}>
-          <a href="https://github.com/LauraCole1900" rel="noreferrer noopener" target="_blank" data-toggle="popover" title="My GitHub"><Image fluid src={GitHubLight} alt="GitHub icon" className="icon" /></a>
-        </Col>
-        <Col xs={1}>
-          <a href="https://drive.google.com/file/d/1gOy3CkufWH_k_Y9dVFQYfZc955bPCT25/view?usp=sharing" rel="noreferrer noopener" target="_blank" data-toggle="popover" title="My Résumé"><Image fluid src={Resume} alt="Résumé icon" className="ltBgFooter" /></a>
-        </Col>
-      </Row>
+      {/* {render === true && */}
+        <Row>
+          <Col sm={{ span: 1, offset: 6 }} className="center">
+            <p>Contact me:</p>
+          </Col>
+          <Col xs={1}>
+            <a href="mailto:lauracole1900@comcast.net" data-toggle="popover" title="Email me: lauracole1900@comcast.net"><Image fluid src={Email} alt="Email icon" className="icon" /></a>
+          </Col>
+          <Col xs={1}>
+            <a href="tel:970-353-0872" data-toggle="popover" title="Call me: 970-353-0872"><Image fluid src={Phone} alt="Phone icon" className="icon" /></a>
+          </Col>
+          <Col xs={1}>
+            <a href="https://www.linkedin.com/in/laura-cole-3661b01b9/?trk=profile-badge" rel="noreferrer noopener" target="_blank" data-toggle="popover" title="My LinkedIn"><Image fluid src={LinkedIn} alt="LinkedIn icon" className="icon" /></a>
+          </Col>
+          <Col xs={1}>
+            <a href="https://github.com/LauraCole1900" rel="noreferrer noopener" target="_blank" data-toggle="popover" title="My GitHub"><Image fluid src={GitHubLight} alt="GitHub icon" className="icon" /></a>
+          </Col>
+          <Col xs={1}>
+            <a href="https://drive.google.com/file/d/1gOy3CkufWH_k_Y9dVFQYfZc955bPCT25/view?usp=sharing" rel="noreferrer noopener" target="_blank" data-toggle="popover" title="My Résumé"><Image fluid src={Resume} alt="Résumé icon" className="ltBgFooter" /></a>
+          </Col>
+        </Row>
+        {/* } */}
     </div >
   )
 }
