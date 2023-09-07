4add23c0ad178801dedb2e78c2b3b9c09646cdbc
Laura Cole <lauracole1900@comcast.net>
2023-09-07 17:15:52 -0600

    added vite-tsconfig-paths package & plugin

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,7 +8,7 @@ export default defineConfig({
   build: {
     outDir: 'build',
   },
-  plugins: [react()],
+  plugins: [react(), tsconfigPaths()],
   server: {
     port: 3000,
     open: true
